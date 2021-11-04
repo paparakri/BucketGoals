d31020bc59fca2113a8bc2bc8f3bfe56a08b0a48
Chris Papakrivopoulos <chrispapakr@gmail.com>
2021-11-04 08:08:52 +0200

    Minor Fixes

--- BucketListProj/src/screens/Home.js
+++ BucketListProj/src/screens/Home.js
@@ -11,22 +11,68 @@ const Home = () => {
                 style={styles.button}
                 onPress={logout}
             >
-                <Text>Log Out</Text>
+                <Text style={styles.text2}>Log Out</Text>
             </TouchableOpacity>
         </View>
     );
 };
 
 const styles = StyleSheet.create({
+    rect: {
+      flex: 1,
+      backgroundColor: "rgba(21,31,40,1)"
+    },
+    text: {
+      color: "rgba(255,255,255,1)",
+      fontSize: 32,
+      lineHeight: 50,
+      textAlign: "left",
+      marginBottom: 20,
+      marginTop: 50,
+      marginLeft: 10,
+      marginRight: 40
+    },
     button: {
-        height: 50,
-        alignSelf:"center",
-        width: 130,
-        backgroundColor: "rgba(29,161,242,1)",
-        borderRadius: 100,
-        justifyContent: "center",
-        marginTop: 60
-      }
-});
+      height: 71,
+      alignSelf:"center",
+      width: 250,
+      backgroundColor: "rgba(29,161,242,1)",
+      borderRadius: 100,
+      justifyContent: "center",
+      marginTop: 40
+    },
+    text2: {
+      color: "rgba(255,255,255,1)",
+      fontSize: 24,
+      alignSelf: "center"
+    },
+    mainColumn: {
+      marginTop: 5,
+      marginLeft: 31,
+      marginRight: 31
+    },
+    textColumnFiller: {
+      flex: 1
+    },
+    rect2: {
+      height: 39,
+      marginBottom: 45,
+      marginLeft: 28,
+      marginRight: 28,
+      marginTop: 20
+    },
+    logo: {
+      marginTop: 10,
+      marginBottom: 80,
+      alignSelf: "center",
+      maxWidth: 130,
+      maxHeight: 130
+    },
+    text3: {
+      color: "rgba(255,255,255,1)",
+      fontSize: 16,
+      marginTop: 13
+    }
+  });
 
 export default Home;
